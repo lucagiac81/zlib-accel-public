77db35fd6956b1fc975e4ee0ad4b951168d26076
Luca Giacchino <luca.giacchino@intel.com>
2025-03-14 12:13:45 -0700

    Add statistics

---
+++ statistics.cpp
@@ -0,0 +1,51 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#ifdef ENABLE_STATISTICS
+
+#include "statistics.h"
+
+#include <thread>
+
+#include "config/config.h"
+#include "logging.h"
+
+// clang-format off
+const std::string stat_names[STATS_COUNT] {
+  "deflate_count",
+  "deflate_error_count",
+  "deflate_qat_count",
+  "deflate_qat_error_count",
+  "deflate_iaa_count",
+  "deflate_iaa_error_count",
+  "deflate_zlib_count",
+
+  "inflate_count",
+  "inflate_error_count",
+  "inflate_qat_count",
+  "inflate_qat_error_count",
+  "inflate_iaa_count",
+  "inflate_iaa_error_count",
+  "inflate_zlib_count"
+};
+// clang-format on
+
+thread_local uint64_t stats[STATS_COUNT];
+
+void PrintStatistics() {
+  if ((stats[DEFLATE_COUNT] + stats[INFLATE_COUNT]) %
+          config::log_stats_samples !=
+      0) {
+    return;
+  }
+
+  std::stringstream printed_stats;
+  printed_stats << "Thread: " << std::this_thread::get_id() << std::endl;
+  for (int i = 0; i < STATS_COUNT; i++) {
+    printed_stats << stat_names[i] << " = " << stats[i] << std::endl;
+  }
+
+  LogStats(printed_stats.str().c_str());
+}
+
+#endif
---
+++ statistics.h
@@ -0,0 +1,47 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#ifdef ENABLE_STATISTICS
+#define INCREMENT_STAT(stat) stats[stat]++
+#define INCREMENT_STAT_COND(cond, stat) \
+  if (cond) stats[stat]++
+#else
+#define INCREMENT_STAT(stat)
+#define INCREMENT_STAT_COND(cond, stat)
+#endif
+
+#ifdef ENABLE_STATISTICS
+#include <atomic>
+#include <map>
+#include <sstream>
+#endif
+
+enum Statistics {
+  DEFLATE_COUNT = 0,
+  DEFLATE_ERROR_COUNT,
+  DEFLATE_QAT_COUNT,
+  DEFLATE_QAT_ERROR_COUNT,
+  DEFLATE_IAA_COUNT,
+  DEFLATE_IAA_ERROR_COUNT,
+  DEFLATE_ZLIB_COUNT,
+
+  INFLATE_COUNT,
+  INFLATE_ERROR_COUNT,
+  INFLATE_QAT_COUNT,
+  INFLATE_QAT_ERROR_COUNT,
+  INFLATE_IAA_COUNT,
+  INFLATE_IAA_ERROR_COUNT,
+  INFLATE_ZLIB_COUNT,
+
+  STATS_COUNT
+};
+
+#ifdef ENABLE_STATISTICS
+extern thread_local uint64_t stats[STATS_COUNT];
+
+void PrintStatistics();
+#else
+#define PrintStatistics(...)
+#endif
