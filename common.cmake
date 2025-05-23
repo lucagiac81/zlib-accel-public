aed17e2772741941a235dc4ded819595d5287093
Luca Giacchino <luca.giacchino@intel.com>
2025-05-22 18:50:26 -0700

    Add undefined behavior sanitizer

--- common.cmake
+++ common.cmake
@@ -7,6 +7,7 @@ option(DEBUG_LOG "for logging" ON)
 option(COVERAGE "for coverage" OFF)
 option(ASAN "Enable AddressSanitizer" OFF)
 option(MSAN "Enable MemorySanitizer" OFF)
+option(UBSAN "Enable UndefinedBehaviorSanitizer" OFF)
 
 if(USE_IAA)
   add_compile_definitions(USE_IAA)
@@ -21,16 +22,24 @@ if(DEBUG_LOG)
 endif()
 
 set(COMPILER_FLAGS "-Wall -Wextra -Werror \
--flto -fvisibility=hidden \
+-fvisibility=hidden \
 -Wformat -Wformat-security -Werror=format-security \
 -D_FORTIFY_SOURCE=2 \
 -fstack-protector-strong")
+# UBSAN not compatible with -flto
+if(NOT UBSAN)
+  set(COMPILER_FLAGS "${COMPILER_FLAGS} -flto")
+endif()
 if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
   message(STATUS "GCC detected.")
   set(COMPILER_FLAGS "${COMPILER_FLAGS} -Wl,-z,noexecstack,-z,relro,-z,now -mindirect-branch-register")
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
   message(STATUS "Clang detected.")
-  set(COMPILER_FLAGS "${COMPILER_FLAGS} -fsanitize=cfi -mretpoline")
+  set(COMPILER_FLAGS "${COMPILER_FLAGS}  -mretpoline")
+  # For UBSAN, -flto is disabled. -fsanitize=cfi depends on it.
+  if(NOT UBSAN)
+    set(COMPILER_FLAGS "${COMPILER_FLAGS} -fsanitize=cfi")
+  endif()
 endif()
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${COMPILER_FLAGS}")
@@ -58,6 +67,11 @@ if(MSAN)
   else()
     message(FATAL_ERROR "Memory sanitizer requires the Clang compiler.")
   endif()
+endif()
+
+if(UBSAN)
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=undefined")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=undefined")
 endif()
 
 set(CMAKE_CXX_STANDARD 17)
