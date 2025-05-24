381744235f3b1cbb25b6fd85c8e13207959b5409
Luca Giacchino <luca.giacchino@intel.com>
2025-05-23 18:26:21 -0700

    Add option to prevent QAT chunking

--- qat.cpp
+++ qat.cpp
@@ -272,6 +272,14 @@ bool SupportedOptionsQAT(int window_bits, uint32_t input_length) {
           __LINE__, input_length);
       return false;
     }
+    if (configs[QAT_COMPRESSION_ALLOW_CHUNKING] &&
+        input_length > QAT_HW_BUFF_SZ) {
+      Log(LogLevel::LOG_INFO,
+          "SupportedOptionsQAT() Line %d input length %d is greater than "
+          "QAT HW buffer and chunking is not allowed\n",
+          __LINE__, input_length);
+      return false;
+    }
     return true;
   }
   return false;
